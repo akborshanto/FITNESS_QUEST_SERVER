a22cc498b84e2338c7d04d625389c951b2b869f5
akbor shanto <akborshanto2002@gmail.com>
2024-06-03 03:07:22 -0700

    payment

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express=require('express')
-const cors= require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const port =process.env.PORT || 5000;
-const app=express()
-app.use(cors())
-app.use(express.json())
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+// This is your test secret API key.
+const stripe = require("stripe")(process.env.SECRET_STRIPE);
 
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+/* stripe */
 /* ========================🚩🚩🚩=========================================
                           MONGODB
 ========================================================================= */
@@ -19,102 +25,154 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful 
-/* COLLECTION */
-const userCollection=  client.db('fitQuest').collection('user')
+    // Send a ping to confirm a successful
+    /* COLLECTION */
+    const userCollection = client.db("fitQuest").collection("user");
+    const newsLetterCollection = client.db("fitQuest").collection("newsLetter");
+    const trainerCollection = client.db("fitQuest").collection("trainer");
+    const becomeTrainerCollection = client
+      .db("fitQuest")
+      .collection("become-trainer");
+    /* USER CNF */
+    const userCnCollection = client.db("fitQuest").collection("userCNF");
+    /* ========================🚩🚩🚩=========================================
+                  STRIPE COLLECTION   
+========================================================================= */
 
-const newsLetterCollection=  client.db('fitQuest').collection('newsLetter')
-const trainerCollection=  client.db('fitQuest').collection('trainer')
-const becomeTrainerCollection=  client.db('fitQuest').collection('become-trainer')
+app.post("/create-payment-intent", async (req, res) => {
+  const { price } = req.body;
+  console.log(price)
+  const amount= parseInt(price * 100)
+  
+console.log(amount)
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+console.log(paymentIntent)
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
 
 
-/* ========================🚩🚩🚩=========================================
+
+
+
+
+
+
+
+    /* ========================🚩🚩🚩=========================================
                         USER COLLECTION   
 ========================================================================= */
 
-app.post('/user',async(req,res)=>{
-const query=req.body;
-console.log(query)
-const result= await userCollection.insertOne(query)
-console.table(result)
+/* get email user */
+app.get('/userCn/:email',async(req,res)=>{
+const email=req.params.email;
+console.log(email)
+const result= await userCnCollection.findOne({email})
 res.send(result)
-
-
 
 
 })
 
 
+/* get all userCn */
 
-/* ========================🚩🚩🚩=========================================
+app.get('/userCn',async (req,res)=>{
+const result= await userCnCollection.find().toArray()
+res.send(result)
+
+
+})
+
+
+    /* USER CNF */
+    app.put("/userCn", async (req, res) => {
+      const user = req.body;
+      /* if user is Exist */
+      const isExist = userCnCollection.findOne({ email: user?.email });
+      if (isExist) {
+        return res.send(isExist);
+      }
+/* savae user firest time */
+      const option = { upsert: true };
+      const query = { email: user?.email };
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+        },
+      };
+
+      const result = await userCnCollection.updateOne(query, updateDoc, option);
+      res.send(result);
+    });
+
+    /* ========================🚩🚩🚩=========================================
                         NEWS LETTER COLLECTION   
 ========================================================================= */
-/* get methos */
-app.get('/news-letter',async (req,res)=>{
-const result= await newsLetterCollection.find().toArray()
-res.send(result)
+    /* get methos */
+    app.get("/news-letter", async (req, res) => {
+      const result = await newsLetterCollection.find().toArray();
+      res.send(result);
+    });
 
-})
-
-
-/* post method */
-app.post('/newsLetter',async (req,res)=>{
-
-const query= req.body;
-console.log(query)
-const result= await newsLetterCollection.insertOne(query)
-res.send(result)
-
-
-
-})
-/* ========================🚩🚩🚩=========================================
+    /* post method */
+    app.post("/newsLetter", async (req, res) => {
+      const query = req.body;
+      console.log(query);
+      const result = await newsLetterCollection.insertOne(query);
+      res.send(result);
+    });
+    /* ========================🚩🚩🚩=========================================
                      TRAINNER BOOKING COLLECTION   
 ========================================================================= */
-app.post('/trainer-booking',async (req,res)=>{
-const qury=req.body;
-console.log(qury)
-const result= await trainerCollection.insertOne(qury)
-res.send(result)
-
-
-})
-/* ========================🚩🚩🚩=========================================
+    app.post("/trainer-booking", async (req, res) => {
+      const qury = req.body;
+      console.log(qury);
+      const result = await trainerCollection.insertOne(qury);
+      res.send(result);
+    });
+    /* ========================🚩🚩🚩=========================================
                         BEOME TRAINER COLLECTION   
 ========================================================================= */
 
-/* get mehtod beacome a trainer */
-app.get('/become-trainer',async(req,res)=>{
-  const result= await becomeTrainerCollection.find().toArray()
-  res.send(result)
-})
+    /* get mehtod beacome a trainer */
+    app.get("/become-trainer", async (req, res) => {
+      const result = await becomeTrainerCollection.find().toArray();
+      res.send(result);
+    });
 
-
-/* post method become a trainer */
-app.post('/become-trainer',async (req,res)=>{
-
-const becomeTrainer=req.body;
-console.log(becomeTrainer)
-const result= await becomeTrainerCollection.insertOne(becomeTrainer)
-res.send(result)
-
-
-})
-
+    /* post method become a trainer */
+    app.post("/become-trainer", async (req, res) => {
+      const becomeTrainer = req.body;
+      console.log(becomeTrainer);
+      const result = await becomeTrainerCollection.insertOne(becomeTrainer);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-  // await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -123,13 +181,10 @@ run().catch(console.dir);
                            
 ========================================================================= */
 
-app.get('/',async (req,res)=>{
+app.get("/", async (req, res) => {
+  res.send("ASSINGMENT 12");
+});
 
-res.send("ASSINGMENT 12")
-
-})
-
-app.listen(port,()=>{
-
-    console.log("ASSINGMENT 12 LINTENTING 12 ")
-})
+app.listen(port, () => {
+  console.log("ASSINGMENT 12 LINTENTING 12 ");
+});
