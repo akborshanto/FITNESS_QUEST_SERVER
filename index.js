a45de2cfa449ff2a303e177fe0f9288a2fb840bc
AKBOR SHANTO <akborshanto11111@gmail.com>
2024-10-05 09:44:56 -0700

    payment router

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173","https://fitness-5ee62.web.app/all-trainer"],
   credentials: true,
   optionSuccessStatus: 200,
 };
@@ -89,11 +89,12 @@ async function run() {
 
 // middlewears ------------------------------------------
 const verifytoken = (req, res, next) => {
-  // console.log('inside verify token', req.headers.authorization);
+ console.log('inside verify token', req.headers.authorization);
   if (!req.headers.authorization) {
       return res.status(401).send({ message: 'unauthorized access' });
   }
   const token = req.headers.authorization.split(' ')[1];
+  console.log(token)
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
       if (err) {
           return res.status(401).send({ message: 'unauthorized access' })
@@ -153,7 +154,7 @@ const verifyTrainer = async (req, res, next) => {
       const result = await userCollect.find().toArray();
       res.send(result);
     });
-    app.get("/fitness/allTrainerNew", verifytoken,verifyAdmin,async (req, res) => {
+    app.get("/fitness/allTrainerNew",async (req, res) => {
       const result = await Trainer.find().toArray();
       res.send(result);
     });
