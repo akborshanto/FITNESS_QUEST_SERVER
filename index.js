772da54146de01b6a92724edb2bfb0cb9af42a80
AKBOR SHANTO <akborshanto11111@gmail.com>
2024-10-08 11:05:21 -0700

    update router

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-// This is your test secret API key.
-const stripe = require("stripe")(process.env.SECRET_STRIPE);
+
 
 const port = process.env.PORT || 5000;
 const app = express();
